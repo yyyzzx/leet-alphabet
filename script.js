71b81a7782f267a533db1f88b64b1f7050090bb2
yyyzzx <yej005@newschool.edu>
2024-10-03 23:52:38 -0400

    add text

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ function addHoverEffectToBox(box, textArray, letter) {
         box.style.fontFamily = "Supply-Regular";
         box.style.color = "black";
         box.style.backgroundColor = "#FFC300";
-        box.style.transition = "1s";
+        box.style.transition = "2s";
+        box.style.position = "relative";
+        box.style.top = "-12px";
+        box.style.left = "-12px";
+        box.style.border = "black solid 0.03rem";
 
         clearInterval(intervalIds[box]); // Stop the interval for this specific box
 
@@ -28,6 +32,9 @@ function addHoverEffectToBox(box, textArray, letter) {
             box.style.fontFamily = "";
             box.style.color = "";
             box.style.backgroundColor = "";
+            box.style.top = "";
+            box.style.left = "";
+            box.style.border = "";
 
             // After 5 seconds, update to a random text immediately
             updateTimeForBox(box, textArray);
@@ -57,31 +64,31 @@ function initializeBoxesForLetter(letter, textArray) {
 // Initialize for all letters in the alphabet
 const alphabet = {
     A: ["4", "@", "/\\", "/-\\"],
-    B: ["8", "|3", "6", "13", "]3"],
-    C: ["<", "(", "{", "[", "€"],
-    D: [")", "|)", "|}", "cl", "|>"],
+    B: ["8", "|3", "6", "13", "[3"],
+    C: ["<", "(", "{", "©", "¢"],
+    D: ["[)", "|)", "|}", "cl"],
     E: ["3", "£", "€", "ë"],
-    F: ["|=", "ph", "||"],
-    G: ["9", "g", "6"],
-    H: ["#","|-|","|-", "]-["],
-    I: ["1", "|", "!", "[]"],
+    F: ["|=", "ƒ", "|#", "/="],
+    G: ["9", "C-", "6", "(_+"],
+    H: ["#","|-|","}{", "]-[", "(-)", "/-/"],
+    I: ["1", "|", "!", "¡"],
     J: ["_|", "_/", "_|"],
     K: ["|<", "|{"],
-    L: ["|_", "1"],
-    M: ["|\\/|", "|\\|\\|", "/\\/\\", "^^"],
-    N: ["|\\|", "/\\/"],
+    L: ["|_", "1_"],
+    M: ["|\\/|", "|\\|\\|", "/\\/\\", "^^", "|v|"],
+    N: ["|\\|", "/\\/", "[\\]", "/V", "^/"],
     O: ["0", "()", "[]"],
-    P: ["|*", "|D", "9"],
-    Q: ["(_,)", "0,"],
-    R: ["|2", "12"],
+    P: ["|*", "|D", "|''", "¶"],
+    Q: ["(_,)", "O_"],
+    R: ["|2", "/2", "®"],
     S: ["5", "$"],
-    T: ["7", "+", "†"],
-    U: ["|_|", "(_)"],
-    V: ["\\/"],
-    W: ["\\/\\/"],
-    X: ["%"],
+    T: ["-|-", "+", "†", "']['"],
+    U: ["|_|", "(_)", "µ"],
+    V: ["\\/", "^"],
+    W: ["\\/\\/", "\\\\'", "\\^/"],
+    X: ["%", "><", ")("],
     Y: ["¥", "'/"],
-    Z: ["2", "5"],
+    Z: ["2", "7_", "~/_"],
 };
 
 // Iterate over each letter in the alphabet and initialize its boxes
